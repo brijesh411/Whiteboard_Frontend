ed87b5d2d23486bb130943dfe57afc542f5ecf12
Brijesh Rajput <rbk4112001@gmail.com>
2023-05-31 17:15:32 +0530

    whiteboard and room page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Forms from './components/Forms/index.jsx'
-
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+
+import Forms from './components/Forms/index.jsx'
+import RoomPage from './pages/RoomPage/RoomPage.jsx'
 
 const App = () => {
   return(
     <div className="container">
-      <Forms />
+      <Routes>
+        <Route path="/" element={<Forms />} />
+        <Route path="/:roomId" element={<RoomPage />} />
+      </Routes>
     </div>
   )
 }

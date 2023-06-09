341622bca5d2ffd773beb5f557234409af60a664
Brijesh Rajput <rbk4112001@gmail.com>
2023-06-09 12:59:16 +0530

    board and drawing feature

--- src/components/Whiteboard/Whiteboard.jsx
+++ src/components/Whiteboard/Whiteboard.jsx
@@ -1,11 +1,86 @@
+import { useEffect, useLayoutEffect, useState } from 'react'
+import rough from 'roughjs/bundled/rough.esm.js' 
+const roughGenerator = rough.generator();
 
+const Whiteboard = ({ canvasRef, ctxRef, elements, setElements }) => {
 
-const Whiteboard = () => {
+  const [isDrawing, setIsDrawing] = useState(false)
+
+  useEffect(() => {
+      
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext("2d")
+
+    ctxRef.current = ctx
+
+  }, [])
+
+  useLayoutEffect(() => {
+    const roughCanvas = rough.canvas(canvasRef.current);
+
+    elements.forEach((element) => {
+      roughCanvas.linearPath(element.path);
+    })
+  },[elements])
+
+  const handleMouseDown = (e) => {
+    const { offsetX, offsetY } = e.nativeEvent
+
+    setElements((prevElements) => [
+      ...prevElements,
+      {
+        type: "pencil",
+        offsetX,
+        offsetY,
+        path: [[offsetX, offsetY]],
+        storke: "black",
+      },
+    ])
+
+    setIsDrawing(true)
+  };
+  
+
+  const handleMouseMove = (e) => {
+    const { offsetX, offsetY } = e.nativeEvent
+    if (isDrawing) {
+
+      const {path} = elements[elements.length - 1];
+      const newPath = [...path, [offsetX, offsetY]];
+
+      setElements((prevElements) => 
+        prevElements.map((ele,index) => {
+          if(index === elements.length-1){
+            return {
+              ...ele,
+              path: newPath,
+            };
+          } else {
+              return ele;
+          }
+        })
+      )
+    }
+  }
+
+  const handleMouseUp = (e) => {
+    setIsDrawing(false)
+    
+  }
+
   return (
-    <canvas className="border border-dark border-3 h-100 w-100" >
-
+    <>
+    <canvas 
+      ref={canvasRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      className="border border-dark border-3 h-100 w-100" >
     </canvas>
+    </> 
   )
 }
 
 export default Whiteboard
+
+
--- src/pages/RoomPage/RoomPage.jsx
+++ src/pages/RoomPage/RoomPage.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Whiteboard from '../../components/Whiteboard/Whiteboard'
 import './RoomPage.css'
 
 const RoomPage = () => {
 
+    const canvasRef = useRef(null)
+    const ctxRef = useRef(null)
+
     const [tool,setTool] = useState("pencil")
     const [color,setColor] = useState("black")
+    const [elements,setElements] = useState([])
 
   return (
     <div className="row">
@@ -78,7 +82,12 @@ const RoomPage = () => {
             </div>
         </div>
         <div className="col-md-10 mx-auto mt-4 canvas-box">
-            <Whiteboard />
+            <Whiteboard 
+            canvasRef={canvasRef} 
+            ctxRef={ctxRef} 
+            elements={elements}
+            setElements={setElements}    
+            />
         </div>
     </div>
   )

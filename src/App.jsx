f9a799c1955ee69b1694580adffee324815784fa
Brijesh Rajput <rbk4112001@gmail.com>
2023-07-22 19:10:07 +0530

    Completed all project features and improvements

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 import Forms from "./components/Forms/index";
 import RoomPage from "./pages/RoomPage/RoomPage";
 
-const server = "http://localhost:5000";
+const server = "https://white-board-backend.onrender.com";
 const connectionOptions = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
